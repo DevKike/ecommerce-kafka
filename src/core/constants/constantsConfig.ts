00e389f92de2030dc7bdd8c0b9e46001f4380d6d
SRT DEV <sahirruiz4@gmail.com>
2025-04-14 01:05:43 -0500

    Refactor: set default value for DB_NAME in constants configuration

--- src/core/constants/constantsConfig.ts
+++ src/core/constants/constantsConfig.ts
@@ -2,12 +2,13 @@ export const CONSTANT_CONFIG = {
   ENVIRONMENT: {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: Number(process.env.PORT) || 3000,
-    DB_NAME: process.env.DB_NAME,
+    DB_NAME: process.env.DB_NAME || 'E-commerceKafka', 
     MONGODB_USERNAME: process.env.MONGODB_USERNAME,
     MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
     MONGODB_HOST: process.env.MONGODB_HOST || 'localhost',
     MONGODB_PORT: Number(process.env.MONGODB_PORT) || 27017,
     LOG_LEVEL: process.env.LOG_LEVEL || 'info',
     HASH_SALT: Number(process.env.HASH_SALT) || 10,
+    JWT_SECRET: process.env.JWT_SECRET
   },
 };
