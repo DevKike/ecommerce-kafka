002ae49a5e8c4af6ab9013a5f0704e8c4e854e31
DevKike <alvaronarvaez.dev@gmail.com>
2025-04-13 23:40:01 -0500

    Refactor: make phone number unique in user schema

--- src/modules/auth/models/userModel.ts
+++ src/modules/auth/models/userModel.ts
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema<IUser>(
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
   },
   { versionKey: false }
 );
