5e064da981f4be46aab61076065f5b2b51f16d81
DevKike <alvaronarvaez.dev@gmail.com>
2025-04-15 21:30:10 -0500

    Refactor: standardize quotes in Inversify configuration and types

--- src/core/inversify/types/inversifyTypes.ts
+++ src/core/inversify/types/inversifyTypes.ts
@@ -1,7 +1,7 @@
 export const TYPES = {
-  RouterManger: Symbol.for("RouterManager"),
-  UserRouter: Symbol.for("UserRouter"),
-  ProductRouter: Symbol.for("ProductRouter"),
+  RouterManger: Symbol.for('RouterManager'),
+  UserRouter: Symbol.for('UserRouter'),
+  ProductRouter: Symbol.for('ProductRouter'),
 
-  Application: Symbol.for("Application"),
+  Application: Symbol.for('Application'),
 };
