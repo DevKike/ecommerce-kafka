cbada03b372949cdc82b30632cb21ea7a03cb23a
SRT DEV <sahirruiz4@gmail.com>
2025-04-15 21:56:48 -0500

    Feat: agregar enlace del CartRouter en la configuración de Inversify

--- src/core/inversify/config/inversifyConfig.ts
+++ src/core/inversify/config/inversifyConfig.ts
@@ -4,12 +4,14 @@ import { RouterManager } from "../../../modules/common/router/RouterManager";
 import { UserRouter } from "../../../modules/auth/routes/UserRouter";
 import { Application } from "../../../app/Application";
 import { ProductRouter } from "../../../modules/products/routes/ProductRouter";
+import { CartRouter } from "../../../modules/cart/routes/CartRouter";
 
 const container = new Container();
 
 container.bind(TYPES.RouterManger).to(RouterManager);
 container.bind(TYPES.UserRouter).to(UserRouter);
 container.bind(TYPES.ProductRouter).to(ProductRouter);
+container.bind(TYPES.CartRouter).to(CartRouter);
 container.bind(TYPES.Application).to(Application);
 
 export { container };
--- src/core/inversify/types/inversifyTypes.ts
+++ src/core/inversify/types/inversifyTypes.ts
@@ -2,6 +2,7 @@ export const TYPES = {
   RouterManger: Symbol.for("RouterManager"),
   UserRouter: Symbol.for("UserRouter"),
   ProductRouter: Symbol.for("ProductRouter"),
+  CartRouter: Symbol.for("CartRouter"),
 
   Application: Symbol.for("Application"),
 };
