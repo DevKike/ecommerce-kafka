9cd1a6dbf9b1958d93d033229022b66f02218ee9
DevKike <alvaronarvaez.dev@gmail.com>
2025-04-16 20:38:47 -0500

    Refactor: add unique constraint to event ID in event model schema

--- src/modules/common/kafka/events/models/eventModel.ts
+++ src/modules/common/kafka/events/models/eventModel.ts
@@ -3,6 +3,7 @@ import { IEvent } from '../interfaces/IEvent';
 
 const eventSchema = new mongoose.Schema<IEvent>(
   {
+    id: { type: String, require: true, unique: true },
     timestamp: { type: String, required: true },
     source: { type: String, required: true },
     topic: { type: String, required: true },
