586ee8c56bd2e13c494e99abde3c8e47cfe35960
SRT DEV <sahirruiz4@gmail.com>
2025-04-16 22:26:22 -0500

    Fix: add cart item to cart before creating update event

--- src/modules/cart/controllers/cartController.ts
+++ src/modules/cart/controllers/cartController.ts
@@ -106,6 +106,8 @@ export const cartController = {
     userId: IUser['id'],
     cartItemData: ICartItemCreate
   ): Promise<void> => {
+    await cartService.addToCart(userId, cartItemData);
+
     await cartController.createCartUpdateEvent(userId, cartItemData);
   },
 
@@ -196,6 +198,4 @@ export const cartController = {
 
     await eventService.save(cartEvent);
   },
-
-  
 };
