6de435f0d02b3bd948277966d447c1b1be27f971
Jared Trujillo <148577531+DevJaredt@users.noreply.github.com>
2025-04-17 01:02:35 -0500

    Feat: add PaymentRouter to RouterManager for order processing

--- src/modules/common/express/router/RouterManager.ts
+++ src/modules/common/express/router/RouterManager.ts
@@ -3,16 +3,19 @@ import { inject, injectable } from 'inversify';
 import { TYPES } from '../../../../core/inversify/types/inversifyTypes';
 import { UserRouter } from '../../../auth/routes/UserRouter';
 import { ProductRouter } from '../../../products/routes/ProductRouter';
+import { PaymentRouter } from '../../../payments/routes/PaymentRouter';
 
 @injectable()
 export class RouterManager {
   constructor(
     @inject(TYPES.UserRouter) private readonly userRouter: UserRouter,
-    @inject(TYPES.ProductRouter) private readonly productRouter: ProductRouter
+    @inject(TYPES.ProductRouter) private readonly productRouter: ProductRouter,
+    @inject(TYPES.PaymentRouter) private readonly paymentRouter: PaymentRouter
   ) {}
 
   manageRoutes(app: Application): void {
     app.use('/api/users', this.userRouter.getRouter());
     app.use('/api/products', this.productRouter.getRouter());
+    app.use('/api/orders', this.paymentRouter.getRouter());
   }
 }
