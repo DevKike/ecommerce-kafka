d3c6484b6175a15592e5e8eae852c58042c6a6b7
Jared Trujillo <148577531+DevJaredt@users.noreply.github.com>
2025-04-17 02:44:30 -0500

    Feat: add FactureRouter binding to Inversify container

--- src/core/inversify/config/inversifyConfig.ts
+++ src/core/inversify/config/inversifyConfig.ts
@@ -5,13 +5,15 @@ import { UserRouter } from '../../../modules/auth/routes/UserRouter';
 import { Application } from '../../../app/Application';
 import { ProductRouter } from '../../../modules/products/routes/ProductRouter';
 import { PaymentRouter } from '../../../modules/payments/routes/PaymentRouter';
+import { FactureRouter } from '../../../modules/payments/routes/FactureRouter';
 
 const container = new Container();
 
 container.bind(TYPES.RouterManger).to(RouterManager);
 container.bind(TYPES.UserRouter).to(UserRouter);
 container.bind(TYPES.ProductRouter).to(ProductRouter);
-container.bind(TYPES.PaymentRouter).to(PaymentRouter); 
+container.bind(TYPES.PaymentRouter).to(PaymentRouter);
+container.bind(TYPES.FactureRouter).to(FactureRouter); 
 container.bind(TYPES.Application).to(Application);
 
 export { container };
--- src/core/inversify/types/inversifyTypes.ts
+++ src/core/inversify/types/inversifyTypes.ts
@@ -3,6 +3,7 @@ export const TYPES = {
   UserRouter: Symbol.for('UserRouter'),
   ProductRouter: Symbol.for('ProductRouter'),
   PaymentRouter: Symbol.for('PaymentRouter'),
+  FactureRouter: Symbol.for('FactureRouter'),
 
   Application: Symbol.for('Application'),
 };
