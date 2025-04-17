a3550ee0d76bf3dfba3a6f5653f4c94df4c4aa25
Jared Trujillo <148577531+DevJaredt@users.noreply.github.com>
2025-04-17 01:02:09 -0500

    Feat: add PaymentRouter binding to Inversify container

--- src/core/inversify/config/inversifyConfig.ts
+++ src/core/inversify/config/inversifyConfig.ts
@@ -4,13 +4,14 @@ import { RouterManager } from '../../../modules/common/express/router/RouterMana
 import { UserRouter } from '../../../modules/auth/routes/UserRouter';
 import { Application } from '../../../app/Application';
 import { ProductRouter } from '../../../modules/products/routes/ProductRouter';
+import { PaymentRouter } from '../../../modules/payments/routes/PaymentRouter';
 
 const container = new Container();
 
 container.bind(TYPES.RouterManger).to(RouterManager);
 container.bind(TYPES.UserRouter).to(UserRouter);
 container.bind(TYPES.ProductRouter).to(ProductRouter);
-
+container.bind(TYPES.PaymentRouter).to(PaymentRouter); 
 container.bind(TYPES.Application).to(Application);
 
 export { container };
--- src/core/inversify/types/inversifyTypes.ts
+++ src/core/inversify/types/inversifyTypes.ts
@@ -2,6 +2,7 @@ export const TYPES = {
   RouterManger: Symbol.for('RouterManager'),
   UserRouter: Symbol.for('UserRouter'),
   ProductRouter: Symbol.for('ProductRouter'),
+  PaymentRouter: Symbol.for('PaymentRouter'),
 
   Application: Symbol.for('Application'),
 };
