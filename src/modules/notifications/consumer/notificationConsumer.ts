cb8383efe8d4fa2b0ea7ab71ced86d4a3ae690c1
SRT DEV <sahirruiz4@gmail.com>
2025-04-17 19:36:18 -0500

    Feat: add invoice consumer and email notification handling for processed invoices

--- src/modules/notifications/consumer/notificationConsumer.ts
+++ src/modules/notifications/consumer/notificationConsumer.ts
@@ -2,6 +2,7 @@ import { kafka } from '../../../core/kafka/kafkaClient';
 import { Logger } from '../../../utils/logger/Logger';
 import { CONSTANT_KAFKA } from '../../common/kafka/constants/constantsKafka';
 import { mailerController } from '../mailer/controllers/mailerController';
+import { mailerService } from '../mailer/services/mailerService';
 
 export const notificationConsumer = kafka.consumer({
   groupId: 'notifications-group',
@@ -16,6 +17,7 @@ export const connectNotificationsConsumer = async () => {
       topics: [
         CONSTANT_KAFKA.TOPIC.USER.WELCOME_FLOW,
         CONSTANT_KAFKA.TOPIC.CART.REMOVED,
+        CONSTANT_KAFKA.TOPIC.NOTIFICATION.EMAIL,
       ],
       fromBeginning: false,
     });
@@ -43,8 +45,21 @@ export const connectNotificationsConsumer = async () => {
                 }
               );
               break;
+            case CONSTANT_KAFKA.TOPIC.NOTIFICATION.EMAIL:
+              await mailerService.sendMail(
+                eventData.payload.to,
+                eventData.payload.subject,
+                'invoice',
+                {
+                  to: eventData.payload.to,
+                  subject: eventData.payload.subject,
+                  content: eventData.payload.content,
+                  date: new Date().toLocaleDateString(),
+                  year: new Date().getFullYear(),
+                }
+              );
+              break;
             default:
-              Logger.warn(`No handler for topic: ${topic}`);
               break;
           }
         } catch (error) {
