56db1f46f775fbfb97b054cc80203a1d581a1597
DevKike <alvaronarvaez.dev@gmail.com>
2025-04-23 04:20:00 -0500

    Refactor environment configuration to use process.env variables and add Kafka settings

--- src/core/constants/constantsConfig.ts
+++ src/core/constants/constantsConfig.ts
@@ -1,14 +1,18 @@
 export const CONSTANT_CONFIG = {
   ENVIRONMENT: {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: Number(process.env.PORT) || 3000,
-    DB_NAME: process.env.DB_NAME || 'E-commerceKafka',
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: Number(process.env.PORT),
+    DB_NAME: process.env.DB_NAME,
     MONGODB_URI: process.env.MONGODB_URI,
-    LOG_LEVEL: process.env.LOG_LEVEL || 'info',
-    HASH_SALT: Number(process.env.HASH_SALT) || 10,
+    LOG_LEVEL: process.env.LOG_LEVEL,
+    HASH_SALT: Number(process.env.HASH_SALT),
     JWT_SECRET: process.env.JWT_SECRET,
     MAIL_SERVICE: process.env.MAIL_SERVICE,
     MAIL_USER: process.env.MAIL_USER,
     MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+    KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID,
+    KAFKA_BROKER: process.env.KAFKA_BROKER,
+    KAFKA_API_KEY: process.env.KAFKA_API_KEY,
+    KAFKA_SECRET: process.env.KAFKA_SECRET,
   },
 };
--- src/core/kafka/kafkaClient.ts
+++ src/core/kafka/kafkaClient.ts
@@ -1,7 +1,14 @@
-import { Kafka } from 'kafkajs';
+import { Kafka, logLevel } from 'kafkajs';
+import { CONSTANT_CONFIG } from '../constants/constantsConfig';
 
 export const kafka = new Kafka({
-  clientId: 'ecommerce-service',
-  brokers: ['localhost:9092'],
+  clientId: CONSTANT_CONFIG.ENVIRONMENT.KAFKA_CLIENT_ID!,
+  brokers: [CONSTANT_CONFIG.ENVIRONMENT.KAFKA_BROKER!],
+  sasl: {
+    mechanism: 'plain',
+    username: CONSTANT_CONFIG.ENVIRONMENT.KAFKA_API_KEY!,
+    password: CONSTANT_CONFIG.ENVIRONMENT.KAFKA_SECRET!,
+  },
+  ssl: true,
+  // logLevel: logLevel.DEBUG,
 });
-
